c6bd77f8383f6eaac8e70665286985d4abe27862
Colyn Prater <cprater@thefrontiergroup.com.au>
2018-12-24 15:05:53 +0800

    Add request and response functionality with hardcoded values.

---
+++ index.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var StringDecoder = require('string_decoder').StringDecoder;
+var url = require('url');
+
+// Instantiate the http server
+var httpServer = http.createServer(function(req, res) {
+  var parsedUrl = url.parse(req.url, true)
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+  if ( trimmedPath == 'hello' ) {
+    res.setHeader('Content-Type', 'application/json')
+    res.writeHead(200);
+
+    var payload = JSON.stringify({ message: 'Hello world' })
+    res.end(payload);
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+});
+
+// Start the http server
+httpServer.listen(3000);
+
+var port = 3000;
