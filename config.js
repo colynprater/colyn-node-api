ebdd5cc319040edd776112a84a356496f220838a
Colyn Prater <cprater@thefrontiergroup.com.au>
2018-12-24 15:20:33 +0800

    Add config file.

---
+++ config.js
@@ -0,0 +1,13 @@
+// Create and exort configuration variables
+// Edit routes and response bodies here.
+
+var config = {
+  routes: {
+    sample: 'hello'
+  },
+  responseObject: {
+    message: 'Hello world'
+  }
+};
+
+module.exports = config;
