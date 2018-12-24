c3df1f459445f550d185a47a12fd89e3ddd51601
Colyn Prater <cprater@thefrontiergroup.com.au>
2018-12-24 15:20:59 +0800

    Replace hardcorded values in main API file with config values.

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-var http = require('http');
+/*
+*
+* Primary API file
+*
+*/
+var config        = require('./config');
+var http          = require('http');
 var StringDecoder = require('string_decoder').StringDecoder;
-var url = require('url');
+var url           = require('url');
 
 // Instantiate the http server
 var httpServer = http.createServer(function(req, res) {
-  var parsedUrl = url.parse(req.url, true)
-  var path = parsedUrl.pathname;
+  var parsedUrl   = url.parse(req.url, true)
+  var path        = parsedUrl.pathname;
   var trimmedPath = path.replace(/^\/+|\/+$/g, '')
 
-  if ( trimmedPath == 'hello' ) {
+  if ( trimmedPath == config.routes.sample ) {
     res.setHeader('Content-Type', 'application/json')
     res.writeHead(200);
 
-    var payload = JSON.stringify({ message: 'Hello world' })
+    var payload = JSON.stringify(config.responseObject)
     res.end(payload);
   } else {
     res.writeHead(404);
@@ -22,5 +28,3 @@ var httpServer = http.createServer(function(req, res) {
 
 // Start the http server
 httpServer.listen(3000);
-
-var port = 3000;
